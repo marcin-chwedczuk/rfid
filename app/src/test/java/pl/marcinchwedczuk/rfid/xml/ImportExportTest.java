8be2d5add8dd6a8317132a7648184b0e2908c669
0xmarcin <0xmarcin+dev@gmail.com>
2021-04-11 12:32:30 +0200

    add junit5

---
+++ app/src/test/java/pl/marcinchwedczuk/rfid/xml/ImportExportTest.java
@@ -0,0 +1,13 @@
+package pl.marcinchwedczuk.rfid.xml;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImportExportTest {
+
+    @Test
+    void toXml() {
+        fail("TODO");
+    }
+}
