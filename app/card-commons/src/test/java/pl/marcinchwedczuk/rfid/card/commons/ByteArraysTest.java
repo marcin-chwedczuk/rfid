0ce680bbd61f559c55ce3b722641f8644fc5ab31
Marcin Chwedczuk <0xmarcin+dev@gmail.com>
2021-06-01 19:40:04 +0200

    Add pitest profile

---
+++ app/card-commons/src/test/java/pl/marcinchwedczuk/rfid/card/commons/ByteArraysTest.java
@@ -0,0 +1,11 @@
+package pl.marcinchwedczuk.rfid.card.commons;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ByteArraysTest {
+
+    public void test() {
+        assertThat(1).isEqualTo(1);
+    }
+}
